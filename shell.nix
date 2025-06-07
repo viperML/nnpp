57f3ac9ec0a0649e83ebde41e6fcad4b8a9b3dd9
Fernando Ayats <ayatsfer@gmail.com>
2025-06-07 11:48:56 +0200

    First commit

---
+++ shell.nix
@@ -0,0 +1,17 @@
+with import <nixpkgs> { };
+mkShell.override { stdenv = clangStdenv; } {
+  packages = [
+    cmake
+    ninja
+    pkg-config
+    neocmakelsp
+    clang-tools
+  ];
+
+  hardeningDisable = ["all"];
+
+  env = {
+    CMAKE_EXPORT_COMPILE_COMMANDS = true;
+    CMAKE_GENERATOR = "Ninja";
+  };
+}
