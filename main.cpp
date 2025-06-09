22f261616cbbcca12519101169c6c42f3d27486a
Fernando Ayats <ayatsfer@gmail.com>
2025-06-09 07:53:36 +0200

    Renames

--- main.cpp
+++ main.cpp
@@ -49,32 +49,32 @@ int main() {
     };
 
     // N0 is images[number]
-    Matrix N1(16, 1);
-    Matrix N2(16, 1);
-    Matrix N3(10, 1);
-    Matrix N3_actual(10, 1);
-    auto Z1 = N1.cloned();
-    auto Z2 = N2.cloned();
-    auto Z3 = N3.cloned();
+    Matrix Layer1(16, 1);
+    Matrix Layer2(16, 1);
+    Matrix Layer3(10, 1);
+    Matrix Layer3_actual(10, 1);
+    auto Activation1 = Layer1.cloned();
+    auto Activation2 = Layer2.cloned();
+    auto Activation3 = Layer3.cloned();
 
     // auto N1_prev = N1.cloned();
     // auto N2_prev = N2.cloned();
     // auto N3_prev = N3.cloned();
 
-    Matrix B1(N1.N, N1.M, init);
-    Matrix B2(N2.N, N2.M, init);
-    Matrix B3(N3.N, N3.M, init);
+    Matrix Bias1(Layer1.N, Layer1.M, init);
+    Matrix Bias2(Layer2.N, Layer2.M, init);
+    Matrix Bias3(Layer3.N, Layer3.M, init);
 
-    Matrix W0(N1.N, images[0].N, init);
-    Matrix W1(N2.N, N1.N, init);
-    Matrix W2(N3.N, N2.N, init);
+    Matrix Weight0(Layer1.N, images[0].N, init);
+    Matrix Weight1(Layer2.N, Layer1.N, init);
+    Matrix Weight2(Layer3.N, Layer2.N, init);
 
-    auto dW0 = W0.clone_seeded(0.0f);
-    auto dW1 = W1.clone_seeded(0.0f);
-    auto dW2 = W2.clone_seeded(0.0f);
-    auto dB1 = B1.clone_seeded(0.0f);
-    auto dB2 = B2.clone_seeded(0.0f);
-    auto dB3 = B3.clone_seeded(0.0f);
+    auto dW0 = Weight0.clone_seeded(0.0f);
+    auto dW1 = Weight1.clone_seeded(0.0f);
+    auto dW2 = Weight2.clone_seeded(0.0f);
+    auto dB1 = Bias1.clone_seeded(0.0f);
+    auto dB2 = Bias2.clone_seeded(0.0f);
+    auto dB3 = Bias3.clone_seeded(0.0f);
 
     auto epochs = std::max(static_cast<size_t>(10000), images.size());
     bool do_break = false;
@@ -84,33 +84,33 @@ int main() {
         auto la = labels[epoch];
 
         for (uint8_t i = 0; i < 10; i++) {
-            N3_actual(i, 0) = (la == i) ? 1.0f : 0.0f;
+            Layer3_actual(i, 0) = (la == i) ? 1.0f : 0.0f;
         }
 
         // Forward propagation
 
         /// First hidden layer
-        W0.multiply_into(im, Z1);
-        Z1.sum_into(B1);
-        Z1.clone_into(N1);
-        N1.apply(sigmoid);
+        Weight0.multiply_into(im, Activation1);
+        Activation1.sum_into(Bias1);
+        Activation1.clone_into(Layer1);
+        Layer1.apply(sigmoid);
 
         /// Second hidden layer
-        W1.multiply_into(N1, Z2);
-        Z2.sum_into(B2);
-        Z2.clone_into(N2);
-        N2.apply(sigmoid);
+        Weight1.multiply_into(Layer1, Activation2);
+        Activation2.sum_into(Bias2);
+        Activation2.clone_into(Layer2);
+        Layer2.apply(sigmoid);
 
         /// Final layer
-        W2.multiply_into(N2, Z3);
-        Z3.sum_into(B3);
-        Z3.clone_into(N3);
-        N3.apply(sigmoid);
+        Weight2.multiply_into(Layer2, Activation3);
+        Activation3.sum_into(Bias3);
+        Activation3.clone_into(Layer3);
+        Layer3.apply(sigmoid);
 
         // Cost calculation
         float cost = 0.0f;
         for (size_t i = 0; i < 10; i++) {
-            auto diff = N3(i, 0) - N3_actual(i, 0);
+            auto diff = Layer3(i, 0) - Layer3_actual(i, 0);
             cost += diff * diff;
         }
         if (cost <= 0.001f) {
@@ -128,46 +128,46 @@ int main() {
         /*
             Calculate dB's
         */
-        N3.elementwise_into(N3_actual, dB3, [](auto n3, auto n3a) {
+        Layer3.elementwise_into(Layer3_actual, dB3, [](auto n3, auto n3a) {
             return n3 - n3a;
         });
 
-        W2.transpose_multiply_into(dB3, dB2);
-        dB2.elementwise_into(Z2, dB2, [](auto left, auto right) {
+        Weight2.transpose_multiply_into(dB3, dB2);
+        dB2.elementwise_into(Activation2, dB2, [](auto left, auto right) {
             return left * sigmoid_derivative(right);
         });
 
-        W1.transpose_multiply_into(dB2, dB1);
-        dB1.elementwise_into(Z1, dB1, [](auto left, auto right) {
+        Weight1.transpose_multiply_into(dB2, dB1);
+        dB1.elementwise_into(Activation1, dB1, [](auto left, auto right) {
             return left * sigmoid_derivative(right);
         });
 
         /*
             Calculate dW's
         */
-        dB3.multiply_transpose_into(N2, dW2);
-        dB2.multiply_transpose_into(N1, dW1);
+        dB3.multiply_transpose_into(Layer2, dW2);
+        dB2.multiply_transpose_into(Layer1, dW1);
         dB1.multiply_transpose_into(im, dW0);
 
         /*
             Apply deltas
         */
-        B3.elementwise_into(dB3, B3, [learning_rate](auto b, auto db) {
+        Bias3.elementwise_into(dB3, Bias3, [learning_rate](auto b, auto db) {
             return b - db * learning_rate;
         });
-        B2.elementwise_into(dB2, B2, [learning_rate](auto b, auto db) {
+        Bias2.elementwise_into(dB2, Bias2, [learning_rate](auto b, auto db) {
             return b - db * learning_rate;
         });
-        B1.elementwise_into(dB1, B1, [learning_rate](auto b, auto db) {
+        Bias1.elementwise_into(dB1, Bias1, [learning_rate](auto b, auto db) {
             return b - db * learning_rate;
         });
-        W2.elementwise_into(dW2, W2, [learning_rate](auto w, auto dw) {
+        Weight2.elementwise_into(dW2, Weight2, [learning_rate](auto w, auto dw) {
             return w - dw * learning_rate;
         });
-        W1.elementwise_into(dW1, W1, [learning_rate](auto w, auto dw) {
+        Weight1.elementwise_into(dW1, Weight1, [learning_rate](auto w, auto dw) {
             return w - dw * learning_rate;
         });
-        W0.elementwise_into(dW0, W0, [learning_rate](auto w, auto dw) {
+        Weight0.elementwise_into(dW0, Weight0, [learning_rate](auto w, auto dw) {
             return w - dw * learning_rate;
         });
 
@@ -179,12 +179,12 @@ int main() {
             // Prediction
             std::cout << "Predicted:\t";
             for (size_t i = 0; i < 10; i++) {
-                std::cout << N3(i, 0) << " ";
+                std::cout << Layer3(i, 0) << " ";
             }
             std::cout << std::endl;
             std::cout << "Actual:\t\t";
             for (size_t i = 0; i < 10; i++) {
-                std::cout << N3_actual(i, 0) << " ";
+                std::cout << Layer3_actual(i, 0) << " ";
             }
         }
 
